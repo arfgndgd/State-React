b2939601694cde9863b43701d56941d313622b2c
arif.gundogdu <arif.gundogdu@isimplatform.io>
2023-07-21 11:20:12 +0300

    app.js function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Course from './Course';
+import { useState } from 'react';
+
+function getRandomCourse() {
+  const courseArray = ['Angular', 'Bootstrap', 'Ccsharp', 'KompleWeb'];
+  return courseArray[Math.floor(Math.random()*courseArray.length)];
+}
 
 function App() {
+  const [courses, setCourses] = useState([])
+  const handleClick = () => {
+    setCourses([...courses, getRandomCourse()])
+
+  }
   return (
     <div className="App">
-      sddsd
+      <button onClick={handleClick}>Kurs Ekle</button>
+      {
+        courses.map((course, index) => {
+          return <Course key={index} courseName={course}/>
+        })
+      }
     </div>
   );
 }
---
+++ src/Course.js
@@ -0,0 +1,15 @@
+import Angular from './images/angular.jpg'
+import Bootstrap from './images/bootstrap5.png'
+import Ccsharp from './images/ccsharp.png'
+import KompleWeb from './images/kompleweb.jpg'
+
+
+function Course() {
+    return (  
+    <div>
+        <img src={Angular}></img>
+    </div>
+    );
+}
+
+export default Course;
